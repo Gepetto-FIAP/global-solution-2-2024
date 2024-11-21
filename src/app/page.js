29e368e402c6a25d6e6a63828938cadf9c58ef22
Guilherme <guilhermeavellar25@gmail.com>
2024-11-21 19:55:21 -0300

    add video

--- src/app/page.js
+++ src/app/page.js
@@ -65,16 +65,16 @@ export default function LandingPage() {
 
       <section className="flex flex-col justify-center items-center bg-[#122417] text-white py-16 relative z-10">
         <h2 className="text-4xl font-bold mb-8">Conheça o nosso propósito</h2>
-        <iframe
-          className="rounded-lg border-0"
-          width="800"
-          height="533.33"
-          src="https://www.youtube.com/embed/hTySFNbMiZo?si=wn_3Vz5vQj_ZqS5C"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        <iframe 
+        width="1120" 
+        height="630" 
+        src="https://www.youtube.com/embed/LQArMszrdiM?si=pvVFYhMXiT7kiF-S" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+        </iframe>
       </section>
       <section className="flex flex-col justify-center items-center bg-[#191919] text-white py-36 relative z-10">
         <h2 className="text-4xl font-bold mb-8">Conheça o nosso time</h2>
